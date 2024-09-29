54883f7dc31381b62c5bc1ee25aeb625f4e84ed9
Joshua Barrass <josh.barrass.work@gmail.com>
2024-09-29 14:40:42 +0100

    Refactor to make getting the info and downloading separate

--- tgdaily/__init__.py
+++ tgdaily/__init__.py
@@ -1,1 +1,1 @@
-from .daily_theme import get_latest_daily_theme, ChecksumError
+from .daily_theme import get_daily_theme_info, download_latest_daily_theme, ChecksumError
--- tgdaily/daily_theme.py
+++ tgdaily/daily_theme.py
@@ -15,7 +15,17 @@ class DailyTheme:
         self.start = start
         self.end = end
 
-def get_latest_daily_theme(outdir=None, overwrite=False):
+    def download(self, outdir=None, overwrite=False):
+        cid = self.cid
+        download_path = download_file(BLOB_URL.format(cid=cid),
+                                      filename=f"{cid}",
+                                      outdir=outdir,
+                                      overwrite=overwrite)
+        if not verify_checksum(download_path):
+            raise ChecksumError("bad checksum on downloaded save")
+        self.path = download_path
+
+def get_daily_theme_info():
     r = requests.get(EVENTS_URL)
     r.raise_for_status()
     j = r.json()
@@ -28,14 +38,9 @@ def get_latest_daily_theme(outdir=None, overwrite=False):
             latest_start = start
             latest_event = event
 
-    cid = event["cid"]
+    return DailyTheme(None, event["cid"], event["start"], event["end"])
 
-    download_path = download_file(BLOB_URL.format(cid=cid),
-                                  filename=f"{cid}",
-                                  outdir=outdir,
-                                  overwrite=overwrite)
-
-    if not verify_checksum(download_path):
-        raise ChecksumError("bad checksum on downloaded save")
-
-    return DailyTheme(download_path, cid, event["start"], event["end"])
+def download_latest_daily_theme(outdir=None, overwrite=False):
+    event = get_daily_theme_info()
+    event.download(outdir=outdir, overwrite=overwrite)
+    return event
