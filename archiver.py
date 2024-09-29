78fad722db2ae26b054ce2531563cd75b29e04a5
Joshua Barrass <josh.barrass.work@gmail.com>
2024-09-29 13:47:42 +0100

    Simple single file downloader

--- archiver.py
+++ archiver.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
+"""archiver.py
 
-pass
+Tiny Glade Daily Theme Archiver.
+
+Usage:
+  archiver.py [-O <outdir>] -s
+
+Args:
+  -s --single           Download the current live daily theme.
+  -O --outdir=<outdir>  Directory to save to [default: .]
+"""
+
+import sys
+import tgdaily
+
+if __name__ == "__main__":
+    from docopt import docopt
+    args = docopt(__doc__)
+    print(args)
+
+    if "--single" in args and args["--single"]:
+        tgdaily.get_latest_daily_theme(outdir=args["--outdir"])
+        sys.exit(0)
